e19184fea927efc0b2234b67e738bee1c7892f05
joe-loach <joe.gloach@gmail.com>
2024-10-14 22:00:57 +0100

    add ParseResult that also holds the source text

--- crates/hand/src/lib.rs
+++ crates/hand/src/lib.rs
@@ -13,23 +13,41 @@ use syntax::SyntaxKind;
 
 #[test]
 fn it_works() {
-    let text = "loop: ADD r0, r1, #1 \n\
-                SUB r0, r0, r1 \n\
-                CMP r0, r1, #1 \n\
-                ADR r1, loop \n\
-                BEQ loop \n\
-                STMDB SP!, {R0-R4, SP} \n\n\
+    let text = "loop: ADD r0, r1, #1\n\
+                SUB r0, r0, r1\n\
+                CMP r0, r1, #1\n\
+                ADR r1, loop\n\
+                BEQ loop\n\
+                STMDB SP!, {R0-R4, SP}\n\n\
                 SUBEQ r0, r1, #5\n\
-                LDR r2, [r3, #1] \n\
-                LDR r2, [r3, r4] \n\
-                LDR r2, [r3, r4, LSL #2] \n\
-                LDR r2, [r3, r4, LSL #2]! \n\
-                LDR r2, [r3, r4, LSL r5]! \n\
-                LDR r2, [r3], r4 \n\
+                LDR r2, [r3]\n\
+                LDR r2, [r3, #1]\n\
+                LDR r2, [r3, r4]\n\
+                LDR r2, [r3, r4, LSL #2]\n\
+                LDR r2, [r3, r4, LSL #2]!\n\
+                LDR r2, [r3, r4, LSL r5]!\n\
+                LDR r2, [r3], r4\n\
                 HLT";
     let text = Arc::<str>::from(text);
-    let frags = HAND::parse(text).unwrap();
+    let frags = parse(text);
     dbg!(frags);
+}
+
+#[derive(Debug)]
+pub struct ParseResult {
+    text: Arc<str>,
+    fragments: Vec<Fragment>,
+    // errors: Vec<Error>?
+}
+
+impl ParseResult {
+    pub fn source(&self) -> &str {
+        &self.text
+    }
+    
+    pub fn fragments(&self) -> &[Fragment] {
+        &self.fragments
+    }
 }
 
 /// loop:
@@ -39,27 +57,25 @@ fn it_works() {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum HAND {}
 
-impl HAND {
-    pub fn parse(text: Arc<str>) -> Result<Vec<Fragment>, ()> {
-        let tree = crate::grammar::parse(text.clone());
-        let root = crate::ast::Root::cast(tree).expect("grammar starts at root");
+pub fn parse(text: Arc<str>) -> ParseResult {
+    let tree = crate::grammar::parse(text.clone());
+    let root = crate::ast::Root::cast(tree).expect("grammar starts at root");
 
-        // TODO: error handling
-        // let mut errors = Vec::new();
-        // crate::ast::validate(root.clone(), &mut errors);
+    // TODO: error handling
+    // let mut errors = Vec::new();
+    // crate::ast::validate(root.clone(), &mut errors);
 
-        let frags = lowering::lower(root /*, &mut errors*/);
+    let fragments = lowering::lower(root /*, &mut errors*/);
 
-        // TODO: error handling
-        // if !errors.is_empty() {
-        //     return Err(Errors {
-        //         src: text.clone(),
-        //         inner: errors,
-        //     });
-        // }
+    // TODO: error handling
+    // if !errors.is_empty() {
+    //     return Err(Errors {
+    //         src: text.clone(),
+    //         inner: errors,
+    //     });
+    // }
 
-        Ok(frags)
-    }
+    ParseResult { text, fragments }
 }
 
 impl rowan::Language for HAND {
