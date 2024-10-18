c9ee0d1ebce72c524611cfff2afc883601e5b8bc
joe-loach <joe.gloach@gmail.com>
2024-10-18 18:45:54 +0100

    add `Match` struct to pair together CIR and value

--- crates/matcher/src/lib.rs
+++ crates/matcher/src/lib.rs
@@ -42,6 +42,31 @@ impl<V> Matcher<V> {
     }
 }
 
+/// Convenience function that wraps matched values together.
+pub fn match_pair<'a, 'b, V>(
+    matcher: &'a Matcher<V>,
+    object: &'b [CIR],
+) -> Option<Match<'a, 'b, V>> {
+    let value = matcher.find_match(object)?;
+    Some(Match { value, matched: object })
+}
+
+#[derive(Debug)]
+pub struct Match<'a, 'b, V> {
+    value: &'a V,
+    matched: &'b [CIR],
+}
+
+impl<'a, 'b, V> Match<'a, 'b, V> {
+    pub fn value(&self) -> &V {
+        self.value
+    }
+    
+    pub fn matched(&self) -> &[CIR] {
+        self.matched
+    }
+}
+
 #[test]
 fn api() {
     use cir::Convert;
